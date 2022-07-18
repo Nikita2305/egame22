6afcd54183a84b6acda3c556e5cc9e469071cf6d
shum117 <shuum117@gmail.com>
2022-07-18 12:35:34 +0600

    initial(WIP) commit for events

---
+++ backend/events.py
@@ -0,0 +1,16 @@
+from backend.model import Model
+
+class Event:
+    def __init__(self):
+        
+
+class EventManager:
+    def __init__(self):
+        
+    def RunEvent(name):
+        
+    def RunDelayedEvent(name, delay):
+        
+    def RunRepeatedEvent(name, period):
+        
+    def SetupTriggeredEvent()
