0004c9b89db7c0958b6f47cfd7763423359ee74e
Ivan <i.morgun@g.nsu.ru>
2022-07-23 05:03:07 +0600

    first vanyas commit (reclassify)

--- server.py
+++ server.py
@@ -84,8 +84,8 @@ Model.GetInstance().wars_ = WarManager(servers,120)
 Model.GetInstance().events_ = EventManager()
 
 Model.GetInstance().news_feed_=NewsFeed(forums)
-for name in forums:
-    Model.ScheduleRoutine(Routine(Floodilka(name),1))
+# for name in forums:
+#     Model.ScheduleRoutine(Routine(Floodilka(name),1))
 
 Model.Run()
 
@@ -201,8 +201,20 @@ async def buy(websocket,token,cur,amount):
         await websocket.send(reply(228,"Not enough crypto",token))
 
 
-async def change_node(node_id, new_state):
-    pass
+async def reclassify(websocket, token, node_id, new_state):
+    Model.AcquireLock()
+    if node_id not in [node.get_id() for node in Model.GetGraph().get_servers_by_owners(Model.GetTeams().GetTeam(token))]:
+        return await websocket.send(reply(208,"not your node",token))
+
+    if not Model.GetTeams.GetTeam(token).AddActionsCheck(-1):
+        return await websocket.send(reply(209,"not enough actions",token))
+    
+    Model.GetGraph().find_server(node_id).set_type(new_state)
+    Model.GetTeams.GetTeam(token).AddActions(-1)
+
+    Model.ReleaseLock()
+
+    await websocket.send(reply(200,"OK",token)) 
 
 async def subscribe_leaderboard(websocket, token):
     def teams(r):
